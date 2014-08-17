6d93ae4b3676bb7850e810d577d4a8a26bdd17ad
Alexey Michurin <a.michurin@gmail.com>
2014-08-17 22:40:19 +0400

    speed up: minimize reflow; remove globals; cosmetics

--- bookmarks-on-new-tab/newtab.js
+++ bookmarks-on-new-tab/newtab.js
@@ -1,4 +1,5 @@
 /*
+ * Ultra light new tab Google chrome extension
  * Copyright (c) 2014 Alexey Michurin <a.michurin@gmail.com>
  * MIT License [http://www.opensource.org/licenses/mit-license.php]
  */
@@ -11,8 +12,6 @@
 
   var storage = chrome.storage.local;
   var document = window.document;
-  var bookmarks; // = chrome.bookmarks // but not now
-  var stoplist;
   var permissions = {permissions: ['bookmarks']};
   var bookmarks_root_element = document.getElementById('bookmarks');
 
@@ -22,54 +21,53 @@
     return e;
   }
 
-  function build_tree(root_id, root_element) {
-    bookmarks.getChildren(root_id, function (nn) {
-      if (nn.length === 0) {
+  function build_tree(root_element, childrens, stoplist) {
+    childrens.forEach(function (v) {
+      var text;
+      if (v.id === '0') { // left root node
+        build_tree(root_element, v.children, stoplist);
         return;
       }
-      nn.forEach(function (v) {
-        var text;
-        if (v.url) {
-          var img = document.createElement('img');
-          img.src = 'chrome://favicon/size/16@1x/' + v.url;
-          var item = div('item');
-          text = div('jail');
-          var label = div('jail-text');
-          label.innerText = v.title;
-          text.appendChild(img);
-          text.appendChild(label);
-          item.title = v.title;
-          item.appendChild(text);
-          item.onclick = function () {
-            window.location.href = v.url;
-          };
-          root_element.appendChild(item);
-        } else { // If v.url is missing, it will be a folder.
-          var folder = div(stoplist[v.id] ? 'closed-folder' : 'folder');
-          var body = div('body');
-          var title = div('title');
-          text = div('jail');
-          text.innerText = v.title;
-          title.appendChild(text);
-          title.onclick = function () {
-            var x = folder.className;
-            var tgt;
-            if (x === 'folder') {
-              tgt = 'closed-folder';
-              stoplist[v.id] = true;
-            } else {
-              tgt = 'folder';
-              delete stoplist[v.id];
-            }
-            storage.set({stoplist: stoplist});
-            folder.className = tgt;
-          };
-          folder.appendChild(title);
-          folder.appendChild(body);
-          root_element.appendChild(folder);
-          build_tree(v.id, body);
-        }
-      });
+      if (v.url) {
+        var img = document.createElement('img');
+        img.src = 'chrome://favicon/size/16@1x/' + v.url;
+        var item = div('item');
+        text = div('jail');
+        var label = div('jail-text');
+        label.innerText = v.title;
+        text.appendChild(img);
+        text.appendChild(label);
+        item.title = v.title;
+        item.appendChild(text);
+        item.onclick = function () {
+          window.location.href = v.url;
+        };
+        root_element.appendChild(item);
+      } else {
+        var folder = div(stoplist[v.id] ? 'closed-folder' : 'folder');
+        var body = div('body');
+        var title = div('title');
+        text = div('jail');
+        text.innerText = v.title;
+        title.appendChild(text);
+        title.onclick = function () {
+          var x = folder.className;
+          var tgt;
+          if (x === 'folder') {
+            tgt = 'closed-folder';
+            stoplist[v.id] = true;
+          } else {
+            tgt = 'folder';
+            delete stoplist[v.id];
+          }
+          storage.set({stoplist: stoplist});
+          folder.className = tgt;
+        };
+        folder.appendChild(title);
+        folder.appendChild(body);
+        root_element.appendChild(folder);
+        build_tree(body, v.children, stoplist);
+      }
     });
   }
 
@@ -77,14 +75,14 @@
     bookmarks_root_element.innerText = '';
     bookmarks_root_element.onclick = undefined;
     bookmarks_root_element.style.cursor = 'default';
-    bookmarks = chrome.bookmarks;
     storage.get({stoplist: null}, function(sl) {
       sl = sl.stoplist;
       if (sl === null) {
         sl = {};
       }
-      stoplist = sl; // set global
-      build_tree('0', bookmarks_root_element);
+      chrome.bookmarks.getTree(function (childrens) {
+        build_tree(bookmarks_root_element, childrens, sl);
+      });
     });
   }
 
